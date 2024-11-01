76f2c8123968e08f3fbbd9410ef0876d3f181862
tizibinvenus <tizibienvenus@gmail.com>
2024-11-01 18:52:06 +0100

    fisrt commit

--- git_push.sh
+++ git_push.sh
@@ -4,4 +4,5 @@ read -p "Entrer le message du commit: " commit_message
 git commit -m "$commit_message"
 git branch -M master
 git remote add origin https://github.com/tizibienvenus/rest_api_with_provider.git
-git push
+git push --set-upstream origin master
+##git push
